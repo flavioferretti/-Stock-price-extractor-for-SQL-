bfce334c6a69d600c6901ab5ca40f85b4b643b40
flavioferretti <flavioferretti@gmail>
2021-05-31 11:50:26 +0200

    vv

--- config/setting.py
+++ config/setting.py
@@ -1,4 +1,4 @@
-db_user = <enter username>
-db_password = <enter password>
-db_name = 'stock' #name of your database. stock for example.
-host= <enter host> # eg: '127.0.0.1'
+db_user = 'flavio'
+db_password = '27182818'
+db_name = 'PriceDB' #name of your database. stock for example.
+host= '192.168.4.1' # eg: '127.0.0.1'
